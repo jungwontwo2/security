213764e6d75139cf3a7e5d97c78d1be4084a4388
Tanguri <jungwontwo2@naver.com>
2024-02-29 16:24:05 +0900

    세션 설정(소멸,중복 로그인, 고정 보호)

--- src/main/java/com/example/testsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/testsecurity/config/SecurityConfig.java
@@ -35,8 +35,18 @@ public class SecurityConfig {
 
 
 
+//        http
+//                .csrf((auth)->auth.disable());
+
+
         http
-                .csrf((auth)->auth.disable());
+                .sessionManagement((auth)->auth.maximumSessions(1)//하나의 아이디에 대한 다중 로그인 허용 개수
+                        .maxSessionsPreventsLogin(true));// 다중 로그인 개수를 초과하였을 경우 처리 방법 true: 새로운 로그인 차단 false: 기존 세션 하나 삭제
+
+
+        //https://substantial-park-a17.notion.site/10-36136f5a91f647b499dbcb5a884aff72
+        http
+                .sessionManagement((session)->session.sessionFixation().changeSessionId());
 
 
         return http.build();
